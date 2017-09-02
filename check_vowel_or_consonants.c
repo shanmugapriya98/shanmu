ccac304b89604c1a0a252ee1f5cbfaadf3899f3d
shanmugapriya98 <31558953+shanmugapriya98@users.noreply.github.com>
2017-09-02 11:32:57 +0530

    Create check_vowel_or_consonants.c

---
+++ check_vowel_or_consonants.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+char character;
+scanf("%c",&charcter);
+if((charcter == 'a') || (charcter == 'e') || (charcter == 'i') || (charcter == 'o') || (charcter == 'u') || (charcter == 'A') || (charcter == 'E') || (charcter == 'I') || (charcter == 'O') || (charcter == 'U'))
+printf("Vowel");
+else
+printf("consonant");
+return 0;
+}
