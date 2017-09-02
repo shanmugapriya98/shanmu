d482de2988c5278e3c3d60bbd96911b038b4727c
shanmugapriya98 <31558953+shanmugapriya98@users.noreply.github.com>
2017-09-02 15:49:32 +0530

    Create armstrong_num.c

---
+++ armstrong_num.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int num,temp1,temp2,sum=0;
+scanf("%d",&num);
+temp1 = num;
+while(temp != 0)
+{
+temp2 = temp1 % 10;
+temp1 = temp1 / 10;
+sum = sum + (temp2 * temp 2 * temp2);
+}
+if(num == sum)
+primtf("Palindrome");
+else
+printf("Not palindrome");
+}
