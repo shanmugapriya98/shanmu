504f723d21f545c7643c96430c915a915d300b4a
shanmugapriya98 <31558953+shanmugapriya98@users.noreply.github.com>
2017-09-02 14:07:55 +0530

    Create sum_natural_num.c

---
+++ sum_natural_num.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int i,sum=0;
+int num_of_ele;
+scanf("%d",&num_of_ele);
+int arr[num_of_ele];
+for(i=0;i<num_of_ele;i++)
+{
+scanf("%d",&arr[num_of_ele]);
+}
+for(i=0;i<num_of_ele;i++)
+{
+sum = sum + arrnum_of_ele];
+}
+printf("%d",sum);
+}
