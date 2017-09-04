1ccc24a3773399da5722cd415ad60443b1cedb6f
shanmugapriya98 <31558953+shanmugapriya98@users.noreply.github.com>
2017-09-04 06:52:12 +0530

    Update armstrong_between_intervals.c

--- armstrong_between_intervals.c
+++ armstrong_between_intervals.c
@@ -1,7 +1,19 @@
 #include<stdio.h>
 int main()
 {
-int start_num,end_num,temp1,temp2,result;
+int i,start_num,end_num,temp1,temp2,result = 0;
 scanf("%d",&start_num);
 scanf("%d",&end_num);
 for(i = start_num;i <= end_num;i++)
+   temp1 = i;
+{
+  while(temp1 != 0)
+  {
+    temp2 = temp1 % 10;
+    temp1 = temp1 / 10;
+    result = result + (temp2 * temp2 * temp2);
+  }
+  if(result == i)
+    printf("%d",i);
+}
+}
