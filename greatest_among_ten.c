28c7226b7aae13e64ca1ffe0e4fae9b0e9361e65
shanmugapriya98 <31558953+shanmugapriya98@users.noreply.github.com>
2017-11-30 14:12:34 +0530

    Create greatest_among_ten.c

---
+++ greatest_among_ten.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int num[10],max,i;
+for(i=0;i<10;i++)
+{
+scanf("%d",&num[i]);
+}
+max = num[0];
+for(i=1;i<10;i++)
+{
+if(max < num[i])
+max = num[i];
+}
+printf("%d is greatest");
+return 0;
+}
